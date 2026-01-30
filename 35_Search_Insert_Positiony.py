a298e1e9384a56d5a22587552b5c84cd6b5eaa97
asdss23 <78685255+asdss23@users.noreply.github.com>
2026-01-30 10:28:34 +0530

    solved it using binary searh
    Commented out an alternative solution using binary search.

--- 35_Search_Insert_Positiony.py
+++ 35_Search_Insert_Positiony.py
@@ -4,3 +4,26 @@ class Solution(object):
             if nums[i] >= target:
                 return i
         return len(nums)
+
+#Solution 2 
+# class Solution(object):
+#     def searchInsert(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         n = len(nums)
+#         ans = n
+#         low = 0
+#         high = n - 1
+#         while low <= high :
+#             mid = (low+high)//2
+
+#             if nums[mid] >= target :
+#                 ans = mid
+#                 high = mid-1
+#             else:
+#                 low = mid+1
+
+        return ans 
